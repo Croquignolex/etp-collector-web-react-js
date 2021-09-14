14d1bb1b95495cba964329cde9cae771d94c22cc
Croquignolex <gpetitalex10@gmail.com>
2021-09-14 13:17:17 +0100

    update collector zione for anonymous refuel

--- src/components/operations/OperationsFleetsAddAnonymousFleetsComponent.js
+++ src/components/operations/OperationsFleetsAddAnonymousFleetsComponent.js
@@ -16,10 +16,12 @@ import {dataToArrayForSelect, mappedSims} from "../../functions/arrayFunctions";
 import {storeAllCollectorSimsRequestReset} from "../../redux/requests/sims/actions";
 import {storeAddAnonymousSupplyRequestReset} from "../../redux/requests/supplies/actions";
 import {applySuccess, requestFailed, requestLoading, requestSucceeded} from "../../functions/generalFunctions";
+import {emitAllZonesFetch} from "../../redux/zones/actions";
 
 // Component
-function OperationsFleetsAddAnonymousFleetsComponent({request, sims, simsRequests, dispatch, handleClose}) {
+function OperationsFleetsAddAnonymousFleetsComponent({request, sims, simsRequests, zones, zonesRequests, dispatch, handleClose}) {
     // Local state
+    const [zone, setZone] = useState(DEFAULT_FORM_DATA);
     const [amount, setAmount] = useState(DEFAULT_FORM_DATA);
     const [receiver, setReceiver] = useState(DEFAULT_FORM_DATA);
     const [directPay, setDirectPay] = useState(true);
@@ -28,6 +30,7 @@ function OperationsFleetsAddAnonymousFleetsComponent({request, sims, simsRequest
 
     // Local effects
     useEffect(() => {
+        dispatch(emitAllZonesFetch());
         dispatch(emitAllCollectorSimsFetch());
         // Cleaner error alert while component did unmount without store dependency
         return () => {
@@ -49,6 +52,11 @@ function OperationsFleetsAddAnonymousFleetsComponent({request, sims, simsRequest
     const handleOutgoingSelect = (data) => {
         shouldResetErrorData();
         setOutgoingSim({...outgoingSim,  isValid: true, data})
+    }
+
+    const handleZoneSelect = (data) => {
+        shouldResetErrorData();
+        setZone({...zone, isValid: true, data})
     }
 
     const handleDirectPaySelect = (data) => {
@@ -76,6 +84,11 @@ function OperationsFleetsAddAnonymousFleetsComponent({request, sims, simsRequest
         return dataToArrayForSelect(mappedSims(sims))
     }, [sims]);
 
+    // Build select options
+    const zoneSelectOptions = useMemo(() => {
+        return dataToArrayForSelect(zones)
+    }, [zones]);
+
     // Reset error alert
     const shouldResetErrorData = () => {
         dispatch(storeAllCollectorSimsRequestReset());
@@ -86,6 +99,7 @@ function OperationsFleetsAddAnonymousFleetsComponent({request, sims, simsRequest
     const handleSubmit = (e) => {
         e.preventDefault();
         shouldResetErrorData();
+        const _zone = requiredChecker(zone);
         const _amount = requiredChecker(amount);
         const _receiver = requiredChecker(receiver);
         const _receiverSim = phoneChecker(receiverSim);
@@ -95,11 +109,15 @@ function OperationsFleetsAddAnonymousFleetsComponent({request, sims, simsRequest
         setReceiver(_receiver);
         setReceiverSim(_receiverSim);
         setOutgoingSim(_outgoingSim);
-        const validationOK = (_amount.isValid && _receiver.isValid && _outgoingSim.isValid && _receiverSim.isValid);
+        const validationOK = (
+            _amount.isValid && _receiver.isValid && _outgoingSim.isValid &&
+            _receiverSim.isValid && _zone.isValid
+        );
         // Check
         if(validationOK) {
             dispatch(emitAddAnonymousSupply({
                 pay: directPay,
+                zone: _zone.data,
                 amount: _amount.data,
                 sim: _outgoingSim.data,
                 receiver: _receiver.data,
@@ -114,6 +132,7 @@ function OperationsFleetsAddAnonymousFleetsComponent({request, sims, simsRequest
         <>
             {requestFailed(request) && <ErrorAlertComponent message={request.message} />}
             {requestFailed(simsRequests) && <ErrorAlertComponent message={simsRequests.message} />}
+            {requestFailed(zonesRequests) && <ErrorAlertComponent message={zonesRequests.message} />}
             <form onSubmit={handleSubmit}>
                 <div className='row'>
                     <div className='col-sm-6'>
@@ -154,6 +173,18 @@ function OperationsFleetsAddAnonymousFleetsComponent({request, sims, simsRequest
                 </div>
                 <div className='row'>
                     <div className='col-sm-6'>
+                        <SelectComponent input={zone}
+                                         label='Zone'
+                                         id='inputZone'
+                                         title='Choisir une zone'
+                                         options={zoneSelectOptions}
+                                         handleInput={handleZoneSelect}
+                                         requestProcessing={requestLoading(zonesRequests)}
+                        />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-sm-6'>
                         <label htmlFor="inputAutoPay">Paiement immédiat?</label>
                         <CheckBoxComponent input={directPay}
                                            id='inputAutoPay'
@@ -172,10 +203,12 @@ function OperationsFleetsAddAnonymousFleetsComponent({request, sims, simsRequest
 // Prop types to ensure destroyed props data type
 OperationsFleetsAddAnonymousFleetsComponent.propTypes = {
     sims: PropTypes.array.isRequired,
+    zones: PropTypes.array.isRequired,
     dispatch: PropTypes.func.isRequired,
     request: PropTypes.object.isRequired,
     handleClose: PropTypes.func.isRequired,
     simsRequests: PropTypes.object.isRequired,
+    zonesRequests: PropTypes.object.isRequired,
 };
 
 export default React.memo(OperationsFleetsAddAnonymousFleetsComponent);
