d59b05fd507c7b44edaabd55d180818d6252af20
Croquignolex <gpetitalex10@gmail.com>
2022-01-10 16:35:15 +0100

    Fix recoveries

--- src/components/recoveries/RecoveriesFleetsCardsComponent.js
+++ src/components/recoveries/RecoveriesFleetsCardsComponent.js
@@ -5,18 +5,25 @@ import LoaderComponent from "../LoaderComponent";
 import OperatorComponent from "../OperatorComponent";
 import FormModalComponent from "../modals/FormModalComponent";
 import {fleetTypeBadgeColor} from "../../functions/typeFunctions";
-import {CANCEL, DONE, PROCESSING} from "../../constants/typeConstants";
 import {dateToString, formatNumber} from "../../functions/generalFunctions";
 import AgentDetailsContainer from "../../containers/agents/AgentDetailsContainer";
+import {AGENT_TYPE, CANCEL, DONE, PROCESSING} from "../../constants/typeConstants";
+import ResourceDetailsContainer from "../../containers/resources/ResourceDetailsContainer";
 
 // Component
 function RecoveriesFleetsCardsComponent({returns, handleCancelModalShow}) {
     // Local states
-    const [agentDetailsModal, setAgentDetailsModal] = useState({show: false, header: "DETAIL DE L'AGENT/RESSOURCE", id: ''});
+    const [agentDetailsModal, setAgentDetailsModal] = useState({show: false, header: "DETAIL DE L'AGENT", id: ''});
+    const [resourceDetailsModal, setResourceDetailsModal] = useState({show: false, header: "DETAIL DE LA RESSOURCE", id: ''});
 
     // Hide agent details modal form
     const handleAgentDetailsModalHide = () => {
         setAgentDetailsModal({...agentDetailsModal, show: false})
+    }
+
+    // Hide resource details modal form
+    const handleResourceDetailsModalHide = () => {
+        setResourceDetailsModal({...resourceDetailsModal, show: false})
     }
 
     // Render
@@ -50,12 +57,17 @@ function RecoveriesFleetsCardsComponent({returns, handleCancelModalShow}) {
                                             <span className="float-right">{item.sim_incoming.number}</span>
                                         </li>
                                         <li className="list-group-item">
-                                            <b>Agent/Ressource</b>
+                                            <b>{(item.agent?.reference === AGENT_TYPE) ? "Agent" : "Ressource"}</b>
                                             <span className="float-right">
                                                 {item.agent.name}
-                                                <i className="fa fa-question-circle small ml-1 hand-cursor text-theme"
-                                                   onClick={() => setAgentDetailsModal({...agentDetailsModal, show: true, id: item.agent.id})}
-                                                />
+                                                {(item.agent?.reference === AGENT_TYPE)
+                                                    ? <i className="fa fa-question-circle small ml-1 hand-cursor text-theme"
+                                                         onClick={() => setAgentDetailsModal({...agentDetailsModal, show: true, id: item.agent.id})}
+                                                    />
+                                                    : <i className="fa fa-question-circle small ml-1 hand-cursor text-theme"
+                                                         onClick={() => setResourceDetailsModal({...resourceDetailsModal, show: true, id: item.agent.id})}
+                                                    />
+                                                }
                                             </span>
                                         </li>
                                         <li className="list-group-item">
@@ -92,6 +104,9 @@ function RecoveriesFleetsCardsComponent({returns, handleCancelModalShow}) {
             {/* Modal */}
             <FormModalComponent modal={agentDetailsModal} handleClose={handleAgentDetailsModalHide}>
                 <AgentDetailsContainer id={agentDetailsModal.id} />
+            </FormModalComponent>
+            <FormModalComponent modal={resourceDetailsModal} handleClose={handleResourceDetailsModalHide}>
+                <ResourceDetailsContainer id={resourceDetailsModal.id} />
             </FormModalComponent>
         </>
     )
